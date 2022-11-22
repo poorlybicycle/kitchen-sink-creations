0a524626842c6d0d8096fc0e5d542316345d4d75
Kristy Vanatta <kristylvanatta@gmail.com>
2022-11-21 20:24:03 -0500

    auth.js file updated

---
+++ utils/auth.js
@@ -0,0 +1,11 @@
+const withAuth = (req, res, next) => {
+
+    if (!req.session.logged_in) {
+        res.redirect('/login');
+    }
+    else {
+        next();
+    }
+};
+
+module.exports = withAuth;
