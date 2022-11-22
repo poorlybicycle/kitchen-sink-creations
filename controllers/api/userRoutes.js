66b82ff75f2d63e414857618b95626f39031f418
Kristy Vanatta <kristylvanatta@gmail.com>
2022-11-21 20:08:31 -0500

    login/logout api routes

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,44 @@
+const router = require('express').Router();
+const { Model } = require('sequelize');
+const { User } = require('../../models');
+
+router.post('/login', async (req, res) => {
+    try {
+        const userData = await User.findOne({ where: { email: req.body.email}});
+
+        if (!userData) {
+            res.status(400).json({ message: 'Incorrect email or password, please try again'});
+            return;
+        }
+
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+        res.status(400).json({ message: 'Incorrect email or password, please try again'});
+        return;
+    }
+
+    req.session.save(() => {
+        req.session.user_id = userData.id;
+        req.session.logged_in = true;
+
+        res.json({ user: userData, message: 'Logged in!'})
+    });
+
+} catch (err) {
+    res.status(400).json(err);
+}
+});
+
+router.post('/logout', (req, res) => {
+    if (req.session.logged_in) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    }
+    else {
+        res.status.apply(404).end();
+    }
+});
+
+module.exports = router;
---
+++ public/js/login.js
@@ -0,0 +1,25 @@
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+
+const email = document.querySelector('#email-login').value.trim();
+const password = document.querySelector('#password-login').value.trim();
+
+if (email && password) {
+    // /api/users/login is just a place holder until we do the api routes
+    const response = await fetch('/api/users/login', {
+        method: 'POST',
+        body: JSON.stringify({ email, password }),
+        headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+        document.location.replace('/');
+    } else {
+        alert('Failed to log in');
+    }
+    }
+};
+
+document
+    .querySelector('.login-form')
+    .addEventListener('submit', loginFormHandler);
