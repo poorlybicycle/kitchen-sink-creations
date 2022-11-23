147b94d8c4cb1e3f56d62b6c9c0b01a410b5dfd5
Mime Jr <petravidovic8@gmail.com>
2022-11-22 19:40:37 -0500

    edit recipes mdoel

--- models/recipes.js
+++ models/recipes.js
@@ -1,2 +1,33 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+
+class Recipe extends Model {
+}
+
+Recipe.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        recipeName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        recipeOrigin: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [6],
+            },
+        },
+    },
+);
+
+module.exports = Recipe;
