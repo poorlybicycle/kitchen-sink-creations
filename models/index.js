fa18097effdee89b896dcd4d64ef94b358cade29
William James <Williamj3795@gmail.com>
2022-11-24 13:47:35 -0500

    import modules, foregin key associations

--- models/index.js
+++ models/index.js
@@ -1,4 +1,25 @@
-const User = require('./User');
+const Ingredient = require('./ingredient');
+const Recipe = require('./recipe');
+const User = require('./user');
+
+// Ingredient belongs to Recipe
+Ingredient.belongsTo(Recipe,{
+    foreignKey: "recipe_id",
+});
+
+// Recipe have many Ingredients
+Recipe.hasMany(Ingredient, {
+    foreignKey: "recipe_id",
+  });
+
+
+
+
+
+
 module.exports = {
-    User
-}
+    Ingredient,
+    Recipe,
+    User,
+  };
+  
