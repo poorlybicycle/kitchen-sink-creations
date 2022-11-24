362866a7ff34e832a60399aa5052a83e89e0b52f
William James <Williamj3795@gmail.com>
2022-11-24 11:44:49 -0500

    models table built for ingrediant

---
+++ models/ingredient.js
@@ -0,0 +1,31 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Ingredient extends Model {
+}
+
+Ingredient.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        ingredient_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    
+    
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'ingredient',
+    }
+);
+
+module.exports = Ingredient;
