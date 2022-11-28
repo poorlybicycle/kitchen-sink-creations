86f5c3986e0036a465b53a6d88fd4d6a6794aba1
poorlybicycle <110194983+poorlybicycle@users.noreply.github.com>
2022-11-28 18:45:00 -0500

    Update recipe.js

--- models/recipe.js
+++ models/recipe.js
@@ -20,6 +20,13 @@ Recipe.init(
             type: DataTypes.STRING,
             allowNull: true,
         },
+        //should be an array of references to ingredients
+        ingredient: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [6],
+            },
     },
     {
         sequelize,
