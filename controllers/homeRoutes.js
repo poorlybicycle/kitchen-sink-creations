072cb16a4bf7f87800bbf8adbe70eaa27e3a0aaf
kmanoovi <ktmandovi@gmail.com>
2022-11-28 19:41:14 -0500

    fixed CSS rendering

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,17 +2,17 @@ const router = require('express').Router();
 const { User } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', withAuth, async (req, res) => {
+router.get('/', async (req, res) => {
     try {
-        const userData = await User.findAll({
-            attributes: { exclude: ['password'] },
-            order: [['name', 'ASC']],
-        });
+        // const userData = await User.findAll({
+        //     attributes: { exclude: ['password'] },
+        //     order: [['name', 'ASC']],
+        // });
 
-        const users = userData.map((project) => project.get({ palin: true }));
+        // const users = userData.map((project) => project.get({ palin: true }));
 
         res.render('homepage', {
-            users,
+            // users,
             logged_in: req.session.logged_in,
         });
     }
