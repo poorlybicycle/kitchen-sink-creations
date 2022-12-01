f02a642b290adb1a967e86f282c302126f70502e
William James <Williamj3795@gmail.com>
2022-12-01 18:44:07 -0500

    changed function

--- public/js/loadRecipe.js
+++ public/js/loadRecipe.js
@@ -1,15 +1,33 @@
 const searchBtn = document.querySelector("#searchRecipes");
-const testArr = ["pizza", "bagels", "bacon (toasted)", "scrambled eggs"]
-function showResults() {
-    document.querySelector("#recipeContainer").innerHTML = "";
-    //perform search and with results, create html
 
-    for(i=0;i<testArr.length; i++) {
-        const newP = document.createElement("p");
-        newP.textContent = testArr[i];
 
-        document.querySelector("#recipeContainer").appendChild(newP)
-    }
+// const testArr = ["pizza", "bagels", "bacon (toasted)", "scrambled eggs"]
+function showResults(event) {
+    fetch('/api/ingredients/newRecipe', {
+        method: "POST",
+        body: JSON.stringify({
+         recipe_name: event.target.dataset.recipe_name  
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    document.location.reload();
 }
+//     document.querySelector("#recipeContainer").innerHTML = `${Recipe}`;
+//     //perform search and with results, create html
+
+//     for(i=0;i<testArr.length; i++) {
+//         const newP = document.createElement("p");
+//         newP.textContent = testArr[i];
+
+//         document.querySelector("#recipeContainer").appendChild(newP)
+//     }
+// }
+
+// function newCard() {
+
+//     document.getElementById("card1").innerHTML = `<img src='img/${firstCard}.png'>"`;
+
 
 searchBtn.addEventListener("click", showResults)
