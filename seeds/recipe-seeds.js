77d025e5677966b4dbe685d4a315418a76015e31
William James <Williamj3795@gmail.com>
2022-11-30 22:22:49 -0500

    recipe seeds js changed property names

--- seeds/recipe-seeds.js
+++ seeds/recipe-seeds.js
@@ -2,46 +2,46 @@ const { Recipe } = require('../models');
 
 const recipeData = [ 
     {
-        title: 'Depression Pie',
-        time: '5 hours',
-        servings: '8',
-        ingredients: 'pie crust, water, sugar, flour, butter, vanilla extract',
+        recipe_name: 'Depression Pie',
+        time: '5',
+        serving: '8',
+        ingredient: 'pie crust, water, sugar, flour, butter, vanilla extract',
         //can I include a link somehow?
         description: 'A Great Depression era water pie recipe. Only requires a few simple ingredients for a creamy, buttery pie!'
     },
     {
-        title: 'Sugar Cookies',
-        time: '30 minutes',
-        servings: '48',
-        ingredients: 'all-purpose flour, baking soda, butter, white sugar, eggs, vanilla extract',
+        recipe_name: 'Sugar Cookies',
+        time: '30',
+        serving: '48',
+        ingredient: 'all-purpose flour, baking soda, butter, white sugar, eggs, vanilla extract',
         description: 'Easy sugar cookies that are soft and chewy.'
     },
     {
-        title: 'Chocolate Chip Cookies',
-        time: '30 minutes',
-        servings: '48',
-        ingredients: 'all-purpose flour, chocolate chips, baking soda, brown sugar, eggs, vanilla extract',
+        recipe_name: 'Chocolate Chip Cookies',
+        time: '30',
+        serving: '48',
+        ingredient: 'all-purpose flour, chocolate chips, baking soda, brown sugar, eggs, vanilla extract',
         description: 'Delicious cookies with crisp edges and chewy middles.'
     },
     {
-        title: 'Muffins',
-        time: '35 minutes',
-        servings: '12',
-        ingredients: 'all-purpose flour, baking soda, salt, white sugar, milk, vegetable oil eggs',
+        recipe_name: 'Muffins',
+        time: '35',
+        serving: '12',
+        ingredient: 'all-purpose flour, baking soda, salt, white sugar, milk, vegetable oil eggs',
         description: 'Simple recipe that can be easily altered for different flavors.'
     },
     {
-        title: 'Muffins',
-        time: '35 minutes',
-        servings: '12',
-        ingredients: 'all-purpose flour, baking soda, salt, white sugar, milk, vegetable oil eggs',
+        recipe_name: 'Muffins',
+        time: '35',
+        serving: '12',
+        ingredient: 'all-purpose flour, baking soda, salt, white sugar, milk, vegetable oil eggs',
         description: 'Simple recipe that can be easily altered for different flavors.'
     },
     {
-        title: 'Easy Bread',
-        time: '2 hours',
-        servings: '2 loaves',
-        ingredients: 'unbleached bread flour, granulated sugar, instant yeast, salt, water, yellow cornmeal',
+        recipe_name: 'Easy Bread',
+        time: '2',
+        serving: '2',
+        ingredient: 'unbleached bread flour, granulated sugar, instant yeast, salt, water, yellow cornmeal',
         description: 'This is the easiest bread recipe you will ever make. In your life.'
     },
 ]
