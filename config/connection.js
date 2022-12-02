9d3fab53fc27935640aa41f729f650c5e0aee5f7
Kristy Vanatta <kristylvanatta@gmail.com>
2022-12-01 19:09:03 -0500

    fixed heroku

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 
 let sequelize;
 
-if (process.env.JAWSB_URL) {
-    sequelize = new Sequelize(process.env.JAWSB_URL);
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
 }
 else {
     sequelize = new Sequelize(
